2ca80ba72e3eb005db6a77152e3bc9df19f4943c
Anna Mashera <anna.mashera@gmail.com>
2016-12-28 18:28:24 +0300

    question change done

--- ap.jsx
+++ ap.jsx
@@ -12,81 +12,51 @@
 
 
 
-
-// я не понимаю структуру данных здесь.
-// если сделать console.log(this) внутри headerHeight, то this окажется data со свойством url: index.html
-// почему? почему не StartPage
-// а если сделать console.log(this) внутри render, то this будет уже StartPage - почему?
-// главный вопрос: как задавать стили, чтобы они были свойствами объекта, и передавались в него, 
-// а за пределами не действовали. или это не нужно? 
-
-
 var data = $.getJSON('index.php', function (data) {
-
+	console.log(typeof(data));
+	class App extends React.Component {
+		
+		constructor(props){
+			super(props);
+			this.state = {data: props.hero }
+		}
 	
-	var App = React.createClass({
-
-		getInitialState: function(){
-			return{
-				data: null
-			};
-		},
-
-
-		handleClick: function(){
+		handleClick () {
 			this.request = $.ajax({
 				url: "process.php",
-				// эти данные будут заполняться по клику на инпуты !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-				data: 	{"qIndex": 2,
+	// эти данные будут заполняться по клику на инпуты!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				data: 	{"qIndex": 1,
 						"505": "start",
 						"next": "next"
 						},
 
 				type: "POST",
-				success: function (result){
-					console.log(this);
-					this.setState({data: result})
+				success: function(result){
+					var resultObject = JSON.parse(result)
+					this.setState({data: resultObject})
 				}.bind(this)
 			});
-		},
-
-		render: function(){
+			console.log(this.state.data);
+		}
+		render () {
 			return(
-				// <div className='hidiv'>
-			 //        <span className = 'hitext'>{this.props.hero.qText}</span>
-				// </div>
-			 //    <div className = 'middleScreen'>
-			 //    	<div className = 'startButton-container'>
-				//         <form action='process.php?qIndex={{question.qIndex}}' method='post'>
-				//         	{% for key, variant in variantes %}
-				//         		<input type = 'hidden' name ="{{key}}" id ="{{key}}" value ='{{variant["answer_text"]}}'>
-				//         	{% endfor %}
-				//         	<button name = "next" id = "next" type= "submit" value = "next" className = "startButton"> Начать </button>
-				//         </form>
-				//     </div>
-			 //        <div className = 'pressEnterDiv'>
-			 //            <span className = 'pressEnterText'> нажмите ENTER </span>
-			 //        </div>
-			 //    </div>
-
-
-
 				<div>
-					<p> {this.state.data} </p>
-					<p> {this.props.hero.qNum} </p>
-					<p> {this.props.hero.qIndex} </p>
-					<p> {this.props.hero.qText} </p>
-					<p> {this.props.hero.qComment} </p>
-					<p> {this.props.hero.qView} </p>
-					<button onClick = {this.handleClick}> Отправка </button>
+					<div className = "header">
+					</div>
+					<div className='content'>
+						<p> {this.state.data.question.id_test} </p>
+						<p> {this.state.data.question.qNum} </p>
+						<p> {this.state.data.question.qIndex} </p>
+						<p> {this.state.data.question.qText} </p>
+						<p> {this.state.data.question.qComment} </p>
+						<button onClick = {this.handleClick.bind(this)}> Отправка </button>
+					</div>
+					<div className = "footer">
+					</div>				
 				</div>
 			)
 		}
-	})
 	
-	ReactDOM.render(<App hero = {data.question}/>, document.getElementById("root"));
-	
+	}
+	ReactDOM.render(<App hero = {data} />, document.getElementById("root"));
 });
-
-
-
