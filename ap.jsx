2c26fb828caee314d992d4dbebb7588ea00846b7
Anna Mashera <anna.mashera@gmail.com>
2016-12-29 17:38:29 +0300

    two first screens

--- ap.jsx
+++ ap.jsx
@@ -11,9 +11,23 @@
 // состояния бывают у сложных компонентов, которые задаются как объекты (используется метод createClass)
 
 
-
 var data = $.getJSON('index.php', function (data) {
-	console.log(typeof(data));
+	
+
+	var Variantes = function (props) {
+		return (
+			<div> 
+			{Object.keys(props.data).map(function(key){ 
+				return  <div key = {key} > 
+							<p> {props.data[key].answerIndex} </p>
+							<p> {props.data[key].answer_text} </p>
+							<input type = "text"/>
+						</div>
+			})}
+			</div>
+		)
+	};
+
 	class App extends React.Component {
 		
 		constructor(props){
@@ -24,39 +38,67 @@ var data = $.getJSON('index.php', function (data) {
 		handleClick () {
 			this.request = $.ajax({
 				url: "process.php",
-	// эти данные будут заполняться по клику на инпуты!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				// эти данные будут заполняться по клику на инпуты!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 				data: 	{"qIndex": 1,
 						"505": "start",
 						"next": "next"
 						},
-
 				type: "POST",
 				success: function(result){
 					var resultObject = JSON.parse(result)
 					this.setState({data: resultObject})
 				}.bind(this)
 			});
-			console.log(this.state.data);
 		}
+
 		render () {
-			return(
-				<div>
-					<div className = "header">
-					</div>
-					<div className='content'>
+			var a = "start";
+			let ContentCondition = null;
+			if (this.state.data.question.qView == 'start'){				
+				ContentCondition = 
+					<div className = 'content'>			
+					    <div className='hidiv'>
+					       	<span className = 'hitext'> {this.state.data.question.qText} </span>
+						</div>
+					    <div className = 'middleScreen'>
+					    	<div className = 'startButton-container'>
+								<form action='' method='post'>
+						        	<button name = "next" id = "next" type= "submit" value = "next" className = "startButton"> Начать </button>
+						        </form>					    
+						    </div>
+					        <div className = 'pressEnterDiv'>
+					            <span className = 'pressEnterText'> нажмите ENTER </span>
+					        </div>
+					    </div>
+					</div>;
+			} else {
+				ContentCondition = 
+					<div className = 'content'>
+						<p> {this.state.data.question.qView} </p>
 						<p> {this.state.data.question.id_test} </p>
 						<p> {this.state.data.question.qNum} </p>
 						<p> {this.state.data.question.qIndex} </p>
 						<p> {this.state.data.question.qText} </p>
 						<p> {this.state.data.question.qComment} </p>
-						<button onClick = {this.handleClick.bind(this)}> Отправка </button>
+						<Variantes data = {this.state.data.variantes}/> 					
+					</div>;
+			}
+
+			return(
+				<div>
+					<div className = "header">
 					</div>
+					{ContentCondition}
+					<div className = 'buttons'>
+						<button onClick = {this.handleClick.bind(this)} > Отправить </button>
+					</div>
+					
 					<div className = "footer">
-					</div>				
+					</div>
 				</div>
 			)
 		}
-	
 	}
+
 	ReactDOM.render(<App hero = {data} />, document.getElementById("root"));
 });
